f7b3aa780519e621106a62eef455e00c8a61a389
muscatelli <muscatelli92@gmail.com>
2021-09-18 00:08:16 +0000

    test-dbapi

---
+++ app.py
@@ -0,0 +1,38 @@
+import os 
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from models import db, User
+from flask_migrate import Migrate
+#from flask_script import Manager
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(BASEDIR, "test.db")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['ENV'] = 'development'
+app.config['DEBUG'] = True
+#manager = Manager(app)
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+@app.route("/")
+def test(): 
+    return jsonify('hola mundo')
+@app.route("/test",methods=["POST"])
+
+def probando():
+    user = User()
+    user.name= request.json.get('name') 
+    user.password= request.json.get('password')
+    user.email= request.json.get('email')
+    user.isActive= request.json.get('isActive')  
+    
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify(user.serialize()), 200
+
+if __name__ == "__main__":
+    app.run(host='localhost',port=8080)
+
+
---
+++ models.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+class User(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(10), nullable=False)
+    email=db.Column(db.String(30), nullable=False)
+    isActive= db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return "<User %r>" % self.id
+
+    def serialize(self):
+        return{
+    'id': self.id,
+    'name': self.name,
+    'password': self.password,
+    'isActive': self.isActive
+}
+
+    def serialize_just_username(self):
+        return{
+    'id':self.id,
+    'name':self.name
+}
