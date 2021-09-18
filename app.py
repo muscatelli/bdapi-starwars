bf6463d3a16d715d22e205f6054e03cc0905a64c
muscatelli <muscatelli92@gmail.com>
2021-09-18 01:20:49 +0000

    fixing-problems

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os 
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from models import db, User
+from models import db, User, Vehicles
 from flask_migrate import Migrate
 #from flask_script import Manager
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
@@ -20,21 +20,39 @@ def test():
     return jsonify('hola mundo')
 @app.route("/test",methods=["POST"])
 
+
 def probando():
     user = User()
-    user.name= request.json.get('name') 
-    user.password= request.json.get('password')
-    user.email= request.json.get('email')
-    user.isActive= request.json.get('isActive')  
+    user.name = request.json.get('name') 
+    user.password = request.json.get('password')
+    user.email = request.json.get('email')
+    user.isActive = request.json.get('isActive')  
     
     db.session.add(user)
     db.session.commit()
 
     return jsonify(user.serialize()), 200
 
+@app.route("/vehicles",methods=["GET"])
+
+def vehicles():
+    vehicles = Vehicles()
+    vehicles.name = request.json.get('name')
+    vehicles.model = request.json.get('model')
+    vehicles_class = request.json.get('vehicles_class')
+    vehicles.crew = request.json.get('crew')
+    vehicles.manufacturer = request.json.get('manufacturer')
+    vehicles.cost_in_credits = request.json.get('cost_in_credits')     
+    
+    db.session.add(vehicles)
+    db.session.commit()
+
+    return jsonify(user.serialize()), 200
+
+
 if __name__ == "__main__":
     app.run(host='localhost',port=8080)
 
-    
+
 
 
--- models.py
+++ models.py
@@ -25,3 +25,26 @@ class User(db.Model):
     'name':self.name
 }
 
+class Vehicles(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    model = db.Column(db.String(10), nullable=False)
+    vehicle_class=db.Column(db.String(30), nullable=False)
+    crews=db.Column(db.String(30), nullable=False)
+    manufacturer=db.Column(db.String(30), nullable=False)
+    cost_in_credits=db.Column(db.String(30), nullable=False)
+    
+
+    def __repr__(self):
+        return "<vehicles %r>" % self.id
+
+    def serialize(self):
+        return{
+    'id': self.id,
+    'name': self.name,
+    'model': self.model,
+    'vechicle_class': self.vechicle_class,
+    'crew': self.crew,
+    'manufacturer': self.manufacturer,
+    'cost_in_credits': self.cost_in_credits
+        }
